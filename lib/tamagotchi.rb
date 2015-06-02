7dbb8745b8db285a8ea377b59b479dbb861d4b2b
audrie <audrie.talbot@gmail.com>
2015-06-02 10:21:53 -0700

    Pass the spec for time_passes method.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,22 +1,31 @@
 class Tamagotchi
   attr_reader(:name)
 
-    define_method(:initialize) do |name|
-      @name = name
-      @food_level = 10
-      @sleep_level = 10
-      @activity_level = 10
-    end
+  define_method(:initialize) do |name|
+    @name = name
+    @age = 0
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
+  end
 
-    define_method(:food_level) do
-      @food_level
-    end
+  define_method(:age) do
+    @age
+  end
 
-    define_method(:sleep_level) do
-      @sleep_level
-    end
+  define_method(:food_level) do
+    @food_level
+  end
 
-    define_method(:activity_level) do
-      @activity_level
+  define_method(:sleep_level) do
+    @sleep_level
+  end
+
+  define_method(:activity_level) do
+    @activity_level
+  end
+
+  define_method(:time_passes) do
+    @food_level = @food_level - 1
   end
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -7,9 +7,17 @@ describe(Tamagotchi) do
     it('sets the name and life levels of a new Tamagotchi') do
       my_pet = Tamagotchi.new('booboo')
         expect(my_pet.name()).to(eq('booboo'))
+        expect(my_pet.age()).to(eq(0))
         expect(my_pet.food_level()).to(eq(10))
         expect(my_pet.sleep_level()).to(eq(10))
         expect(my_pet.activity_level()).to(eq(10))
+    end
+  end
+  describe('#time_passes') do
+    it('decreases the amount of food the Tamagotchi has left by 1') do
+      my_pet = Tamagotchi.new('booboo')
+      my_pet.time_passes()
+      expect(my_pet.food_level()).to(eq(9))
     end
   end
 end
